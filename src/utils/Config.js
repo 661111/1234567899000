a0f7a2fc4accbbc9544953bc02de8eb9d47dee65
wuyin <108344166+661111@users.noreply.github.com>
2022-10-29 20:18:35 +0800

    Update Config.js

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,6 +1,6 @@
 let DefaultConfig = {
   // private_api_url: 'https://fcircle.yyyzyyyz.cn',
-  private_api_url: 'http://wyblog1.eu.org/',
+  private_api_url: 'https://wyblog.eu.org/',
   public_api_url: "https://fcircle-pub.rct.cool/",
   page_turning_number: 10,
   error_img: "https://sdn.geekzu.org/avatar/57d8260dfb55501c37dde588e7c3852c",
@@ -13,8 +13,8 @@ export default DefaultConfig;
 //   jsonurl: '',
 //   apiurl: '',
 //   apipublieurl: 'https://fcircle-pub.rct.cool/', //默认公共库
-//   initnumber: 20,  //首次加载文章数
-//   stepnumber: 10,  //更多加载文章数
+//   initnumber: 20000,  //首次加载文章数
+//   stepnumber: 1000,  //更多加载文章数
 //   article_sort: 'created', //文章排序 updated or created
 //   error_img: 'https://sdn.geekzu.org/avatar/57d8260dfb55501c37dde588e7c3852c'
 // }
